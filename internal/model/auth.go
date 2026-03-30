4d7bf9075f3fbb3444b0e62c48395fb7d716641e
mahardikakdenie <kucingemas744@gmail.com>
2026-03-30 11:32:53 +0700

    feat(auth-model): create model AUTH for type response and Request

---
+++ internal/model/auth.go
@@ -0,0 +1,17 @@
+package model
+
+type RegisterRequest struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+	User  User   `json:"user"`
+}
