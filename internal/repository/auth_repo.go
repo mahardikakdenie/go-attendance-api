4b1b26dc3172f774c95fa3ba54706b9c104762bf
mahardikakdenie <kucingemas744@gmail.com>
2026-03-30 12:51:32 +0700

    feat(auth-repo): create repository and initial for authentication

---
+++ internal/repository/auth_repo.go
@@ -0,0 +1,32 @@
+package repository
+
+import (
+	"go-attendance-api/internal/model"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(user *model.User) error
+	FindByEmail(email string) (model.User, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{
+		db: db,
+	}
+}
+
+func (r *userRepository) Create(user *model.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) FindByEmail(email string) (model.User, error) {
+	var user model.User
+	err := r.db.Where("email = ?", email).First(&user).Error
+	return user, err
+}
