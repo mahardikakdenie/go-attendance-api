0a6355f3db107ef5c447c6cbdc0d8a75eaa1a1c6
mahardikakdenie <kucingemas744@gmail.com>
2026-03-30 12:49:51 +0700

    feat(user-model): adjustmemnt refactor change languange to english

--- internal/model/user.go
+++ internal/model/user.go
@@ -2,15 +2,12 @@ package model
 
 import "time"
 
-// User (Tabel Karyawan)
 type User struct {
-	ID       uint   `gorm:"primaryKey" json:"id"`
-	Nama     string `gorm:"type:varchar(100);not null" json:"nama"`
-	Email    string `gorm:"type:varchar(100);unique;not null" json:"email"`
-	Password string `gorm:"type:varchar(255);not null" json:"-"`
-
+	ID          uint         `gorm:"primaryKey" json:"id"`
+	Name        string       `gorm:"type:varchar(100);not null" json:"name"`
+	Email       string       `gorm:"type:varchar(100);unique;not null" json:"email"`
+	Password    string       `gorm:"type:varchar(255);not null" json:"-"`
 	Attendances []Attendance `gorm:"foreignKey:UserID" json:"attendances,omitempty"`
-
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt   time.Time    `json:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at"`
 }
