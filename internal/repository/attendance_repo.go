b2ce96d38afeb87dec9c093d121f00be7b36b482
mahardikakdenie <kucingemas744@gmail.com>
2026-04-01 09:46:48 +0700

    feat(attendance-repo): create function for findAll data attendances:

--- internal/repository/attendance_repo.go
+++ internal/repository/attendance_repo.go
@@ -1,8 +1,6 @@
 package repository
 
 import (
-	"time"
-
 	"go-attendance-api/internal/model"
 
 	"gorm.io/gorm"
@@ -10,8 +8,9 @@ import (
 
 type AttendanceRepository interface {
 	Save(attendance *model.Attendance) error
-	Update(attendance *model.Attendance) error
 	FindTodayByUser(userID uint) (model.Attendance, error)
+	Update(attendance *model.Attendance) error
+	FindAll(filters ...model.AttendanceFilter) ([]model.Attendance, error)
 }
 
 type attendanceRepository struct {
@@ -28,17 +27,32 @@ func (r *attendanceRepository) Save(attendance *model.Attendance) error {
 	return r.db.Create(attendance).Error
 }
 
+func (r *attendanceRepository) FindTodayByUser(userID uint) (model.Attendance, error) {
+	var attendance model.Attendance
+	err := r.db.Where("user_id = ? AND DATE(clock_in_time) = CURRENT_DATE", userID).First(&attendance).Error
+	return attendance, err
+}
+
 func (r *attendanceRepository) Update(attendance *model.Attendance) error {
 	return r.db.Save(attendance).Error
 }
 
-func (r *attendanceRepository) FindTodayByUser(userID uint) (model.Attendance, error) {
-	var attendance model.Attendance
+func (r *attendanceRepository) FindAll(filters ...model.AttendanceFilter) ([]model.Attendance, error) {
+	var attendances []model.Attendance
+	query := r.db.Preload("User").Model(&model.Attendance{})
 
-	now := time.Now()
-	startOfDay := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	if len(filters) > 0 {
+		filter := filters[0]
 
-	err := r.db.Where("user_id = ? AND clock_in_time >= ?", userID, startOfDay).First(&attendance).Error
+		if filter.UserID != 0 {
+			query = query.Where("user_id = ?", filter.UserID)
+		}
 
-	return attendance, err
+		if filter.Status != "" {
+			query = query.Where("status = ?", filter.Status)
+		}
+	}
+
+	err := query.Find(&attendances).Error
+	return attendances, err
 }
