61283318b918188f4c0491297cef4d6c98d45180
mahardikakdenie <kucingemas744@gmail.com>
2026-04-03 22:47:52 +0700

    feat: extend attendance filter model for include and paging state

--- internal/model/attendance.go
+++ internal/model/attendance.go
@@ -69,6 +69,9 @@ type AttendanceResponse struct {
 type AttendanceFilter struct {
 	UserID   uint
 	Status   AttendanceStatus
+	limit    int
+	offset   int
+	includes []string
 	DateFrom *time.Time
 	DateTo   *time.Time
 }
