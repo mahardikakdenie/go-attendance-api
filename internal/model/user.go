a6d65191507e1a4932dd32191a33a70cb786a0fb
mahardikakdenie <kucingemas744@gmail.com>
2026-04-05 12:53:01 +0700

    feat(users): update and adjustment validasi for swagger

--- internal/model/user.go
+++ internal/model/user.go
@@ -11,23 +11,23 @@ const (
 )
 
 type User struct {
-	ID       uint `gorm:"primaryKey" json:"id"`
+	ID       uint `gorm:"primaryKey" json:"id" example:"1"`
 	TenantID uint `gorm:"index;not null" json:"tenant_id" example:"1"`
 
 	Tenant *Tenant `gorm:"foreignKey:TenantID" json:"tenant,omitempty"`
 
-	Name     string `gorm:"type:varchar(100);not null" json:"name" example:"Budi Santoso"`
-	Email    string `gorm:"type:varchar(100);unique;not null" json:"email" example:"budi@company.com"`
-	Password string `gorm:"type:varchar(255);not null" json:"-"`
+	Name     string `gorm:"type:varchar(100);not null" json:"name" example:"Budi Santoso" binding:"required,min=3,max=100"`
+	Email    string `gorm:"type:varchar(100);unique;not null" json:"email" example:"budi@company.com" binding:"required,email"`
+	Password string `gorm:"type:varchar(255);not null" json:"-"` // tetap hidden
 
-	Role UserRole `gorm:"type:varchar(50);default:employee" json:"role" example:"employee"`
+	Role UserRole `gorm:"type:varchar(50);default:employee" json:"role" example:"employee" binding:"omitempty,oneof=admin manager employee"`
 
 	Attendances []Attendance `gorm:"foreignKey:UserID" json:"attendances,omitempty"`
 
-	MediaUrl string `gorm:"type:varchar(50);" json:"media_url"`
+	MediaUrl string `gorm:"type:varchar(255)" json:"media_url" example:"https://cdn.example.com/profile/budi.jpg" binding:"omitempty,url"`
 
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at" example:"2026-04-05T12:00:00Z"`
+	UpdatedAt time.Time `json:"updated_at" example:"2026-04-05T12:30:00Z"`
 }
 
 type UserFilter struct {
