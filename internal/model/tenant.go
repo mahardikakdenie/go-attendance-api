61daf2ce7be6a63c307a34ebe6776954a542027a
mahardikakdenie <kucingemas744@gmail.com>
2026-04-07 20:08:38 +0700

    feat(model): update tenant model

--- internal/model/tenant.go
+++ internal/model/tenant.go
@@ -3,14 +3,16 @@ package model
 import "time"
 
 type Tenant struct {
-	ID        uint   `gorm:"primaryKey"`
-	Name      string `gorm:"not null"`
-	Code      string `gorm:"uniqueIndex"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID             uint           `gorm:"primaryKey"`
+	Name           string         `gorm:"not null"`
+	Code           string         `gorm:"uniqueIndex"`
+	TenantSettings *TenantSetting `gorm:"foreignKey:TenantID" json:"tenant_settings,omitempty"`
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
 }
 
 type TenantResponse struct {
-	ID   uint   `json:"id"`
-	Name string `json:"name"`
+	ID             uint           `json:"id"`
+	Name           string         `json:"name"`
+	TenantSettings *TenantSetting `json:"tenant_settings,omitempty"`
 }
