b7b4152b51f416e3ce60268ad437338d0ea9a2e3
mahardikakdenie <kucingemas744@gmail.com>
2026-04-16 21:39:51 +0700

    feat(database): added payrols and attendance correnction tables

--- internal/config/database.go
+++ internal/config/database.go
@@ -37,7 +37,7 @@ func InitDB() *gorm.DB {
 	if os.Getenv("RESET_DB") == "true" {
 		log.Println("⚠️ Resetting database tables...")
 		// Disable FK checks for clean drop if possible or drop in strict reverse order
-		db.Exec("DROP TABLE IF EXISTS employee_lifecycle_tasks, lifecycle_tasks, employee_rosters, holidays, work_shifts, support_messages, provisioning_tickets, trial_requests, leaves, leave_balances, leave_types, attendances, overtimes, user_change_requests, recent_activities, tokens, media, users, role_hierarchies, role_permissions, permissions, roles, tenant_settings, tenants CASCADE")
+		db.Exec("DROP TABLE IF EXISTS attendance_corrections, employee_lifecycle_tasks, lifecycle_tasks, employee_rosters, holidays, work_shifts, support_messages, provisioning_tickets, trial_requests, leaves, leave_balances, leave_types, attendances, overtimes, user_change_requests, recent_activities, tokens, media, users, role_hierarchies, role_permissions, permissions, roles, tenant_settings, tenants CASCADE")
 		log.Println("⚠️ Semua tabel berhasil di-reset (CASCADE)")
 	}
 
@@ -85,6 +85,8 @@ func InitDB() *gorm.DB {
 			&model.SupportMessage{},
 			&model.EmployeeRoster{},
 			&model.EmployeeLifecycleTask{},
+			&model.AttendanceCorrection{},
+			&model.Payroll{},
 		)
 		if err != nil {
 			log.Fatalf("❌ Gagal migrasi Stage 3: %v", err)
