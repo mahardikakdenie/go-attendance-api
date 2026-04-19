3329edd38690ab2d199b50a64a3daf17d5f7cf4d
mahardikakdenie <kucingemas744@gmail.com>
2026-04-19 15:52:25 +0700

    fix(swag): fiixng swagger init

--- internal/config/redis.go
+++ internal/config/redis.go
@@ -10,9 +10,19 @@ import (
 var Ctx = context.Background()
 
 func NewRedis() *redis.Client {
+	redisAddr := getEnv("REDIS_ADDR", "127.0.0.1:6379")
+
+	if len(redisAddr) >= 8 && (redisAddr[:8] == "redis://" || (len(redisAddr) >= 9 && redisAddr[:9] == "rediss://")) {
+		opts, err := redis.ParseURL(redisAddr)
+		if err != nil {
+			panic("failed to parse redis url: " + err.Error())
+		}
+		return redis.NewClient(opts)
+	}
+
 	return redis.NewClient(&redis.Options{
-		Addr:     getEnv("REDIS_ADDR", "127.0.0.1:6379"),
-		Password: "", // kalau ada password isi disini
+		Addr:     redisAddr,
+		Password: getEnv("REDIS_PASSWORD", ""), // kalau ada password isi disini
 		DB:       0,
 	})
 }
